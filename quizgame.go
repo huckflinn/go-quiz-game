ead01b43658043198d35fcba689a050ac31ef32d
Nicholas Flinn <nick@Mercury.local>
2025-02-01 11:51:54 -0500

    Read CSV

--- quizgame.go
+++ quizgame.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	f, err := os.ReadFile("./QuizInput.csv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := csv.NewReader(strings.NewReader(string(f)))
+
+	var solutions uint8
+
+	var numRight uint8
+
+	for {
+		row, err := r.Read()
+
+		if err == io.EOF {
+			fmt.Printf("Total Questions: %d\n", solutions)
+			fmt.Printf("Correct Answers: %d\n", numRight)
+			fmt.Println("Thanks for playing! Goodbye!")
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(row[0])
+	}
 }
